3017def0d3c3bd35e89a5a870bc803cf9772492d
Vivek Mandal <vivek.mandal@shipyaari.com>
2023-09-25 01:11:17 +0530

    let and var difference

--- var_and_let.js
+++ var_and_let.js
@@ -2,33 +2,29 @@
 
   ❓ Difference between var and let 
 
-  ✔ The keywords let and var both declare new variables in JavaScript. The difference between let and var is in the scope of the variables they create:
-  ✔ Variables declared by let are only available inside the block where they're defined.
-  ✔ Variables declared by var are available throughout the function in which they're declared.
+  ☝ The keywords let and var both declare new variables in JavaScript. The difference between let and var is in the scope of the variables they create:
+  ☝ Variables declared by let are only available inside the block where they're defined.
+  ☝ Variables declared by var are available throughout the function in which they're declared.
 
 */
 
 // Examples
 var i = 1;
-
 // Created a scope using {}
 {
   var i = 2;
   console.log(i); // Output: 2
 }
-
 console.log(i) // Output: 2
 
 /////////////////////////////
 
 let j = 3;
-
 // Created a scope using {}
 {
   let j = 4;
   console.log(j); // Output: 4
 }
-
 console.log(j) // Output: 3
 
 
